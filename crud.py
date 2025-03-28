2154bb402cc0a9933146df90b27e83ceefbb6938
mariets <hello3_14world@mail.ru>
2025-03-28 15:20:40 +0300

    comments on code in crud.py

--- crud.py
+++ crud.py
@@ -2,12 +2,15 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 
+# получение информации о пользователе по id
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+# получение информации о пользователе по email
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+# регистрация нового пользователя
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password, full_name=user.full_name, phone=user.phone)
@@ -16,9 +19,11 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
     return db_user
 
+# получение информации о корте по id
 def get_court(db: Session, court_id: int):
     return db.query(models.Court).filter(models.Court.id == court_id).first()
 
+# создание нового бронирования
 def create_booking(db: Session, booking: schemas.BookingBase):
     db_booking = models.Booking(**booking.dict())
     db.add(db_booking)
@@ -26,6 +31,7 @@ def create_booking(db: Session, booking: schemas.BookingBase):
     db.refresh(db_booking)
     return db_booking
 
+# отмена бронирования
 def cancel_booking(db: Session, booking_id: int):
     db_booking = db.query(models.Booking).filter(models.Booking.id == booking_id).first()
     if db_booking is None:
